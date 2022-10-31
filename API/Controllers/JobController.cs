1444b63f3d4e361e2af6220ef7b13a59b3181c4a
Michael Murzakov <mikemurzakov@live.com>
2022-10-31 18:42:41 +0200

    basic logging added

--- API/Controllers/JobController.cs
+++ API/Controllers/JobController.cs
@@ -3,6 +3,7 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using API.DTOs;
 using AutoMapper;
+using NLog;
 
 namespace API.Controllers
 {
@@ -10,9 +11,10 @@ namespace API.Controllers
     [ApiController]
     public class JobController : ControllerBase
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
         private readonly IJobStatisticsRepository _repo;
         private readonly IMapper _mapper;
-
         public JobController(IJobStatisticsRepository repo, IMapper mapper)
         {
             this._repo = repo;
@@ -22,6 +24,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<JobStatisticsDto>>> GetJobStatisticsByDateRange(DateTime startDate, DateTime endDate)
         {
+            _logger.Info($"Received request for statistics between [{startDate.ToShortDateString}] and [{endDate.ToShortDateString}].");
+
             var statistics = await _repo.GetJobsStatisticsByDateRangeAsync(startDate, endDate);
             return Ok(_mapper
                 .Map<IReadOnlyList<JobStatistics>, IReadOnlyList<JobStatisticsDto>>(statistics));
